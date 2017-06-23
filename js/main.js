26c421699d51341db5ac8c831b354b24d8f4836a
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-06-23 06:27:55 -0400

    add first commit
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let inputTextboxMessage = document.getElementById("message");
+    let inputTextboxAuthor = document.getElementById("author");
+    inputTextboxMessage.addEventListener('keydown', (function(e) {
+        preventEnter(e);
+        getMessage(inputTextboxMessage);
+        sendData();
+    }));
+    inputTextboxAuthor.addEventListener('keydown', (function(e) {
+        preventEnter(e);
+        getAuthor(inputTextboxAuthor);
+    }));
+});
+
+function getMessage(inputTextboxMessage) {
+    console.log(inputTextboxMessage.value);
+    inputTextboxMessage.addEventListener('keyup', (function(e) {
+        let searchUrl = `https://publicmessage.herokuapp.com/api/v0/message/${inputTextboxMessage.value}`;
+        console.log(searchUrl);
+        let request = new XMLHttpRequest();
+        request.open('GET', searchUrl, true);
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                // Success!
+                let data = JSON.parse(this.response);
+                console.log(data);
+                $('#resultMessage').html(`Someone posted <br> ${data.message} <br> with message id of ${data.id} and author id of ${data.author_id}`);
+            } else {
+                // We reached our target server, but it returned an error
+                console.log(`Something is wrong with query ${inputTextboxMessage.value}`);
+                $('#resultMessage').html(`The application encountered an error. If ${inputTextboxMessage.value} is a valid message number, please send me a message on signal at +1 661 434 5874. No calls please.`);
+            }
+        };
+        request.onerror = function() {
+            // There was a connection error of some sort
+        };
+        request.send();
+    }));
+}
+
+function getAuthor(inputTextboxAuthor) {
+    console.log(inputTextboxAuthor);
+    inputTextboxAuthor.addEventListener('keyup', (function(e) {
+        let searchUrl = `https://publicmessage.herokuapp.com/api/v0/author/${inputTextboxAuthor.value}`;
+        console.log(searchUrl);
+        let request = new XMLHttpRequest();
+        request.open('GET', searchUrl, true);
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                // Success!
+                let data = JSON.parse(this.response);
+                console.log(data);
+                $('#resultAuthor').html(`Some information about author with id ${data.id} follows: <br> Full name: ${data.full_name} <br> Email: ${data.email}`);
+            } else {
+                // We reached our target server, but it returned an error
+                console.log(`Something is wrong with query ${inputTextboxAuthor.value}`);
+                $('#resultAuthor').html(`The application encountered an error. If ${inputTextboxAuthor.value} is a valid author number, please send me a message on signal at +1 661 434 5874. No calls please.`);
+            }
+        };
+        request.onerror = function() {
+            // There was a connection error of some sort
+        };
+        request.send();
+    }));
+}
+
+function preventEnter(e) {
+    if (e.which == 13) {
+        console.log("Please don't press enter. Be kind to the ajax.");
+        event.preventDefault();
+        return false;
+    }
+}
+
+function sendData() {
+    $.ajax({
+        url: 'https://publicmessage.herokuapp.com/api/v0/message/',
+        type: 'POST',
+        data: {
+            json: JSON.stringify({
+                message_id: 2,
+                author_id: 1,
+                message: "你好"
+            })
+        },
+        dataType: 'json'
+    });
+}
